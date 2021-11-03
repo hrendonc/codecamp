0aca4fa1da2ea0f257a16d9354728e403c4e828e
Hector Rendon <hrchilpo@gmail.com>
2021-11-03 13:52:14 -0600

    Sirviendo un archivo html y uno estatico

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+// Configuraciones
+puerto = '3000'
+absolutePath = __dirname + "/views/index.html"
+
+// Middleware
+app.use("/public", express.static(__dirname + "/public"))
+
+//Rutas
+app.get("/", (req, res)=>{
+    res.sendFile(absolutePath)
+})
+
+/*
+app.get("/", (req, res)=>{
+    res.send("Hello Express by ecThor")
+})
+*/
+/*
+app.get("json", (req, res)=>{
+    res.json({
+        
+    })
+})
+*/
+
+app.listen(puerto, function(){
+    console.log('Escuchando en puerto 3000')
+})
+
+module.exports = app
